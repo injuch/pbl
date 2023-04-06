d8fdf108af360e0b7228e64c5442d87dbda1df16
injuch <dlswn2002@naver.com>
2023-04-06 19:15:54 +0900

    Initial commit

---
+++ src/main/java/com/example/GroupBuying/GroupBuyingApplication.java
@@ -0,0 +1,13 @@
+package com.example.GroupBuying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupBuyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupBuyingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GroupBuying/GroupBuyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GroupBuying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupBuyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
